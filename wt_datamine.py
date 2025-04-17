94ebbca7b51807895bfdf450dfb7240312fc5fb5
Sergey Titkov <Srg.titkov@gmail.com>
2025-04-17 14:35:02 +0300

    Принял важное решение, сравнивать будем в одних единицах, приведу эталонные данные к 5 знакам после запятой и вот тогда будем сравнивать
    Очень много всего наделал, надо мельчить комиты

--- wt_datamine.py
+++ wt_datamine.py
@@ -34,11 +34,14 @@ class units_name:
         else:
             raise KeyError(f"Ключ '{key}' не найден.")
 
+
 units_name = units_name()
 
-# Класс возрващает иформацию о самолете
+
+# Класс возвращает информацию о самолете
 class plane_datamine:
-    # Определям тип самолета
+
+    # Определяем тип самолета.
     def _get_type(self, json_data):
         result = ''
         # тут начались танцы с бубном, е...кие.
@@ -48,13 +51,13 @@ class plane_datamine:
 
         # Тип смотрим на то как себя ведет он по аишному, причем может вести себя сильно по разному :) списочком
         if 'fightAiBehaviour' in json_data:
-           if isinstance(json_data['fightAiBehaviour'], list):
-               for item in json_data['fightAiBehaviour']:
-                   if item in json_types:
-                    result = item.replace('type', '').lower()
-                    break
-           else:
-              result = json_data['fightAiBehaviour']
+            if isinstance(json_data['fightAiBehaviour'], list):
+                for item in json_data['fightAiBehaviour']:
+                    if item in json_types:
+                        result = item.replace('type', '').lower()
+                        break
+            else:
+                result = json_data['fightAiBehaviour']
 
         # Продолжаем искать
         if result not in etalon_types:
@@ -62,10 +65,10 @@ class plane_datamine:
                 if isinstance(json_data['type'], list):
                     for item in json_data['type']:
                         if item in json_types:
-                            result = item.replace('type','').lower()
+                            result = item.replace('type', '').lower()
                             break
                 else:
-                    result = json_data['type'].replace('type','').lower()
+                    result = json_data['type'].replace('type', '').lower()
 
         # Проверям финальный вариант
         if result not in etalon_types:
@@ -89,6 +92,244 @@ class plane_datamine:
             result = self.id
         return result
 
+    def _get_length(self, json_data):
+        """Метод возвращает длину самолета, если атрибут не найден возвращает 0
+        """
+        result = 0
+        if 'Length' in json_data:
+            result = json_data['Length']
+        else:
+            logging.warning(f'Самолет:{self.id} - длину не нашли')
+        return result
+
+    def _wing_span(self, json_data):
+        """Метод возвращает массив пар значений в которых записан размах крыла самолета в зависимости от того насколько у него разложено крыло.
+        Для обычных самолетов возвращается массив вида [[0,<размах крыла>]], получить значение wing_span[0][1]
+        """
+        result = []
+        default = [0,0]
+        if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'Span' in json_data['Aerodynamics']['WingPlane']:
+            default[1] = json_data['Aerodynamics']['WingPlane']['Span']
+            result.append(default)
+        else:
+            if 'Wingspan' in json_data:
+                default[1] = json_data['Wingspan']
+                result.append(default)
+            else:
+              # Бывает и изменяемая стреловидность
+              if 'Aerodynamics' in json_data and 'WingPlaneSweep0' in json_data['Aerodynamics']:
+                for i in range(0, 5):
+                    row = [0,0]
+                    if f'WingPlaneSweep{i}' in json_data["Aerodynamics"]:
+                        row[0] = json_data["Aerodynamics"][f'WingPlaneSweep{i}']['Sweep']
+                        row[1] = json_data["Aerodynamics"][f'WingPlaneSweep{i}']['Span']
+                        result.append(row)
+              else:
+                logging.warning(f'Самолет:{self.id} - размах крыла не нашли')
+
+        return result
+
+    def _wing_area(self, json_data):
+        """Метод возвращает площадь крыла, если не удалось посчитать то возвращаем 0
+        """
+        result = 0
+        if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'Areas' in json_data['Aerodynamics']['WingPlane']:
+            result = json_data['Aerodynamics']['WingPlane']["Areas"]["LeftIn"] + json_data['Aerodynamics']['WingPlane']["Areas"]["LeftMid"] + \
+                     json_data['Aerodynamics']['WingPlane']["Areas"]["LeftOut"] + json_data['Aerodynamics']['WingPlane']["Areas"]["RightIn"] + \
+                     json_data['Aerodynamics']['WingPlane']["Areas"]["RightMid"] + json_data['Aerodynamics']['WingPlane']["Areas"]["RightOut"]
+        else:
+            if 'Areas' in json_data:
+                result = json_data["Areas"]["WingLeftIn"] + json_data["Areas"]["WingLeftMid"] + json_data["Areas"]["WingLeftOut"] + json_data["Areas"][
+                    "WingRightIn"] + json_data["Areas"]["WingRightMid"] + json_data["Areas"]["WingRightOut"]
+            else:
+                logging.warning(f'Самолет:{self.id} - площадь крыла не нашли')
+        return result
+
+    def _empty_mass(self, json_data):
+        """Метод возвращает сухую массу самолета, если не удалось посчитать то возвращаем 0
+        """
+        result = 0
+        if 'Mass' in json_data and 'EmptyMass' in json_data['Mass']:
+            result = int(json_data['Mass']['EmptyMass'])
+        else:
+            logging.warning(f'Самолет:{self.id} - сухую массу не нашли')
+        return result
+
+    def _max_fuel_mass(self, json_data):
+        """Метод возвращает максимальное количество топлива.
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if 'Mass' in json_data and 'MaxFuelMass0' in json_data['Mass']:
+            result = int(json_data['Mass']['MaxFuelMass0'])
+        else:
+            logging.warning(f'Самолет:{self.id} - максимальную массу топлива не нашли')
+        return result
+
+    def _crit_air_spd(self, json_data):
+        """Метод возвращает критическую скорость для самолёта в км/ч
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'Strength' in json_data['Aerodynamics']['WingPlane']:
+            result = int(json_data['Aerodynamics']['WingPlane']['Strength']['VNE'])
+        else:
+            if 'Vne' in json_data:
+                result = int(json_data['Vne'])
+            else:
+                logging.warning(f'Самолет:{self.id} - критическую скорость в км/час не нашли')
+        return result
+
+    def _crit_air_spd_mach(self, json_data):
+        """Метод возвращает критическую скорость для самолёта в махах
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'Strength' in json_data['Aerodynamics']['WingPlane']:
+            result = (json_data['Aerodynamics']['WingPlane']['Strength']['MNE'])
+        else:
+            if 'VneMach' in json_data:
+                result = (json_data['VneMach'])
+            else:
+                logging.warning(f'Самолет:{self.id} - критическую скорость в махах не нашли')
+        return result
+
+    def _crit_gear_spd(self, json_data):
+        """Метод возвращает критическую скорость ВЫПУСКА шасси
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if 'Mass' in json_data and 'GearDestructionIndSpeed' in json_data['Mass']:
+            result = int(json_data['Mass']['GearDestructionIndSpeed'])
+        else:
+            logging.warning(f'Самолет:{self.id} - скорость разрушения шасси не нашли')
+        return result
+
+    def _flaps(self, json_data):
+        """Метод возвращает позицию закрылок для самолета в процентах
+        Словарь имеет вид: {'Combat':16, 'Takeoff':19}
+        Если определить параметр не удалось, то возвращаем пустой словарь
+        """
+        result = {}
+        if 'Aerodynamics' in json_data and "FlapsAxis" in json_data['Aerodynamics']:
+            if json_data['Aerodynamics']["FlapsAxis"]["Combat"]["Presents"]:
+                result['Combat'] = int(json_data['Aerodynamics']["FlapsAxis"]["Combat"]["Flaps"] * 100)
+
+            if json_data['Aerodynamics']["FlapsAxis"]["Takeoff"]["Presents"]:
+                result['Takeoff'] = int(json_data['Aerodynamics']["FlapsAxis"]["Takeoff"]["Flaps"] * 100)
+        else:
+            logging.warning(f'Самолет:{self.id} - позиций закрылок не нашли')
+        return result
+
+    def _crit_flaps_spd(self, json_data):
+        """Метод возвращает критическую скорость для позиции закрылок самолета в процентах
+        Словарь имеет вид: {'Combat':16, 'Takeoff':19}
+        Если определить параметр не удалось, то возвращаем пустой словарь
+        """
+        result = {}
+        #CritFlapsSpd = ''
+        #for i in range(0, 5):
+        #     if "Mass" in json_data and f'FlapsDestructionIndSpeedP{i}' in json_data["Mass"]:
+        #         if isinstance(json_data["Mass"][f'FlapsDestructionIndSpeedP{i}'][0], float):
+        #             CritFlapsSpd = f'{CritFlapsSpd},{json_data["Mass"][f'FlapsDestructionIndSpeedP{i}'][0]},{int(json_data["Mass"][f'FlapsDestructionIndSpeedP{i}'][1])}'
+        #         else:
+        #             print(f'{short_file_name} - требуется уточнение по критическим скоростям')
+        #             CritFlapsSpd = f'{CritFlapsSpd},{json_data["Mass"][f'FlapsDestructionIndSpeedP{i}'][0][0]},{int(json_data["Mass"][f'FlapsDestructionIndSpeedP{i}'][0][1])}'
+        #     else:
+        #         logging.warning(f'Самолет:{self.id} - требуется уточнение по критическим скоростям')
+        #         CritFlapsSpd = ''
+        #
+        # cvs_row_data['CritFlapsSpd'] = CritFlapsSpd
+        return result
+
+    def _crit_wing_overload(self, json_data):
+        """Метод возвращает скорость отваливания крыла самолета в км/ч
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'Strength' in \
+                json_data['Aerodynamics']['WingPlane']:
+            result = f'{json_data["Aerodynamics"]["WingPlane"]["Strength"]["CritOverload"][0]},{json_data["Aerodynamics"]["WingPlane"]["Strength"]["CritOverload"][1]}'
+        else:
+            if 'Mass' in json_data and "WingCritOverload" in json_data["Mass"]:
+                result = f'{json_data["Mass"]["WingCritOverload"][0]},{json_data["Mass"]["WingCritOverload"][1]}'
+            else:
+                logging.warning(f'Самолет:{self.id} - скорость слома крыла не нашли')
+        return result
+
+    def _num_engines(self, json_data):
+        """Метод возвращает количество двигателей в самолете
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = int(0)
+        if "Engine0" in json_data:
+            result += 1
+
+        if "Engine1" in json_data:
+            result += 1
+
+        return result
+
+    def _rpm(self, json_data):
+        """Метод возвращает набор максимальных значений оборота двигателя, параметры читаюстся по первому двигателю :)
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = {}
+        if "EngineType0" in json_data:
+            result['RPMMin'] = int(json_data["EngineType0"]["Main"]["RPMMin"])
+            result['RPMAfterburner'] =int(json_data["EngineType0"]["Main"]["RPMAfterburner"])
+            result['RPMMaxAllowed'] = int(json_data["EngineType0"]["Main"]["RPMMaxAllowed"])
+        else:
+            logging.warning(f'Самолет:{self.id} - обороты двигателя не нашли')
+        return result
+
+    def _max_nitro(self, json_data):
+        """Метод возвращает хрен его знает
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if "Mass" in json_data and "MaxNitro" in json_data["Mass"]:
+            result = json_data["Mass"]["MaxNitro"]
+        else:
+            logging.warning(f'Самолет:{self.id} - нитро не нашли')
+        return result
+
+    def _nitro_consum(self, json_data):
+        """Метод возвращает хрен его знает
+        Если определить параметр не удалось, то возвращаем 0
+        """
+        result = 0
+        if "EngineType0" in json_data:
+            result = json_data["EngineType0"]["Afterburner"]["NitroConsumption"]
+        else:
+            logging.warning(f'Самолет:{self.id} - форсаж на закисии озота не нашли')
+        return result
+
+    def _crit_aoa(self, json_data):
+        """Метод возвращает критические перегрузки
+        Если определить параметр не удалось, то {}
+        """
+        result = {}
+        # if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and 'FlapsPolar0' in \
+        #         json_data['Aerodynamics']['WingPlane'] and 'Aerodynamics' in json_data and 'WingPlane' in json_data[
+        #     'Aerodynamics'] and 'FlapsPolar1' in json_data['Aerodynamics']['WingPlane']:
+        #     cvs_row_data[
+        #         'CritAoA'] = f'{int(json_data["Aerodynamics"]["WingPlane"]["FlapsPolar0"]["alphaCritLow"])},{int(json_data["Aerodynamics"]["WingPlane"]["FlapsPolar1"]["alphaCritHigh"])},{int(json_data["Aerodynamics"]["WingPlane"]["FlapsPolar1"]["alphaCritLow"])}'
+        # else:
+        #     if 'Aerodynamics' in json_data and 'NoFlaps' in json_data['Aerodynamics']:
+        #         cvs_row_data[
+        #             'CritAoA'] = f'{int(json_data["Aerodynamics"]["NoFlaps"]["alphaCritLow"])},{int(json_data["Aerodynamics"]["FullFlaps"]["alphaCritHigh"])},{int(json_data["Aerodynamics"]["FullFlaps"]["alphaCritLow"])}'
+        #     else:
+        #         if 'Aerodynamics' in json_data and 'WingPlane' in json_data['Aerodynamics'] and "Polar" in \
+        #                 json_data["Aerodynamics"]["WingPlane"]:
+        #             cvs_row_data[
+        #                 'CritAoA'] = f'{int(json_data["Aerodynamics"]["WingPlane"]["Polar"]["NoFlaps"]["alphaCritLow"])},{int(json_data["Aerodynamics"]["WingPlane"]["Polar"]["FullFlaps"]["alphaCritHigh"])},{int(json_data["Aerodynamics"]["WingPlane"]["Polar"]["FullFlaps"]["alphaCritLow"])}'
+        #         else:
+        #             print(f'{short_file_name} - критические углы не нашли')
+        #             cvs_row_data['CritAoA'] = ''
+        return result
+
+    # Загружаем данные из флайт модели при создании объекта
     def __init__(self, plane_name):
         self.id = plane_name
         full_file_name = f'{flightmodels_path}\\{self.id}.blkx'
@@ -96,5 +337,27 @@ class plane_datamine:
         with open(full_file_name, 'r') as file:
             main_data = json.load(file)
             self.flight_model = self._get_flight_model(main_data)
-            self.name = {'English':units_name[f'{self.id}_0']}
-            self.type = self. _get_type(main_data)
+            self.name = {'English': units_name[f'{self.id}_0']}
+            self.type = self._get_type(main_data)
+
+            # Читаем данные из конкретной флайт модели для самолета.
+            fm_file_name = f'{flightmodels_path}\\fm\\{self.flight_model}.blkx'
+            with open(fm_file_name, 'r') as fm_file:
+                # Прочитали данные из флайт модели
+                fm_data = json.load(fm_file)
+                self.length = self._get_length(fm_data)
+                self.wing_span = self._wing_span(fm_data)
+                self.wing_area = self._wing_area(fm_data)
+                self.empty_mass = self._empty_mass(fm_data)
+                self.max_fuel_mass = self._max_fuel_mass(fm_data)
+                self.crit_air_spd = self._crit_air_spd(fm_data)
+                self.crit_air_spd_mach = self._crit_air_spd_mach(fm_data)
+                self.crit_gear_spd = self._crit_gear_spd(fm_data)
+                self.flaps = self._flaps(fm_data)
+                self.crit_flaps_spd = self._crit_flaps_spd(fm_data)
+                self.crit_wing_overload = self._crit_wing_overload(fm_data)
+                self.num_engines = self._num_engines(fm_data)
+                self.rpm = self._rpm(fm_data)
+                self.max_nitro = self._max_nitro(fm_data)
+                self.nitro_consum = self._nitro_consum(fm_data)
+                self.crit_aoa = self._crit_aoa(fm_data)
--- z.py
+++ z.py
@@ -1,31 +1,69 @@
 import csv
 
-# Пример данных для записи
-cvs_name = [
-    {'Name': 'il-2m_mstitel', 'FmName': 'il-2m_mstitel', 'Type': 'strike', 'English': 'IL-2M "Avenger"'}
+
+import csv
+
+# Пример данных
+data = [
+    [0.1234, 2.56789, 3.0],
+    [4.5, 5.678, 6.9999],
+    [7.123456, 8.9, 9.1111],
+    [14, 15.0, 16.0001]  # Пример с целыми числами
 ]
 
-# Сохраняем все в файлы
-with open('fm_names_db.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Name', 'FmName', 'Type', 'English']
+with open('numbers.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for row in data:
+        formatted_row = []
+        for num in row:
+            # Форматируем число с тремя знаками после запятой
+            s = f"{num:.3f}"
+            # Убираем .000 для целых чисел
+            if s.endswith('.000'):
+                s = s[:-4]
+            formatted_row.append(s)
+        writer.writerow(formatted_row)
 
-    # Создаем объект writer
-    writer = csv.DictWriter(csvfile, dialect='excel', delimiter=';', fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
+print("Данные успешно записаны в numbers.csv")
 
-    # Записываем заголовок
-    writer.writeheader()
+list_row = []
+header = []
+with open(f'fm_data_db_etalon.csv', newline='', encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';')
+    is_header = True;
+    for row in spamreader:
+        if is_header:
+            header = row
+            is_header = False;
+        else:
+            list_row.append(row)
 
-    # Записываем тело, обходя кавычки
-    for row in cvs_name:
-        # Выбираем значения, не экранируя кавычки, для нужного формата
-        writer.writerow({
-            'Name': row['Name'],
-            'FmName': row['FmName'],
-            'Type': row['Type'],
-            'English': row['English']  # Здесь кавычки уже находятся как нужно
-        })
+with open(f'fm_data_db_etalon_.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    # Магия что бы excel открыл файл нормально
+    writer = csv.writer(csvfile, dialect='excel', delimiter=';')
+    writer.writerow(header)
+    # Записываем тело
+    for row in list_row:
+        row[1] = f"{float(row[1]):.5f}"
 
-# Для проверки содержимого файла
-with open('fm_names_db.csv', 'r', encoding='utf-8') as csvfile:
-    print(csvfile.read())
+        if row[2].find(',') ==-1:
+            row[2] = f"{float(row[2]):.5f}"
+        else:
+            lists = row[2].split(',')
+            result = ''
+            for item in lists:
+                result = f'{result},{float(item):.1f}'
+            row[2] = result[1:]
 
+        row[4] = f"{float(row[4]):.5f}"
+        row[5] = f"{float(row[5]):.5f}"
+        row[8] = f"{float(row[8]):.5f}"
+        row[9] = f"{float(row[9]):.5f}"
+        row[10] = f"{float(row[10]):.5f}"
+        row[13] = f"{float(row[13]):.5f}"
+        row[15] = f"{float(row[15]):.5f}"
+        row[16] = f"{float(row[16]):.5f}"
+
+        writer.writerow(row)
+
+#print(list_row)
